699eb04a5141d26ddfa9071899169aed08a5ca7d
PennyJiang <PennyJiang@asus>
2020-03-20 03:38:03 +0800

    second commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,10 @@
 
 public class Hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello everyone,");
+    System.out.print(" I am studing in Feng Chia University.");
+  }
 
 }
